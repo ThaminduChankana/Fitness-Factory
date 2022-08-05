bbb4c27432c27e633ac3a7480bf844604b5a25d7
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-08-05 14:27:28 +0530

    configured MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const connectDB = require("./config/db");
 
+dotenv.config();
+connectDB();
 app.use(express.json());
 app.use("*", cors());
 
