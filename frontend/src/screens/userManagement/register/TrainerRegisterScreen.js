4f469118c1a4cc3530f433f0b462a33b39b8ab1b
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-08-14 17:06:04 +0530

    created trainer registration screen

---
+++ frontend/src/screens/userManagement/register/TrainerRegisterScreen.js
@@ -0,0 +1,340 @@
+import { useState } from "react";
+import { Form, Button, Row, Col, Card } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loading from "../../../components/Loading";
+import ErrorMessage from "../../../components/ErrorMessage";
+import { trainerRegister } from "../../../actions/trainerActions";
+import MainScreen from "../../../components/MainScreen";
+
+const TrainerRegisterScreen = () => {
+	const [name, setName] = useState("");
+	const [dob, setDob] = useState("");
+	const [nic, setNic] = useState("");
+	const [gender, setGender] = useState("");
+	const [telephone, setTelephone] = useState("");
+	const [address, setAddress] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmpassword, setConfirmPassword] = useState("");
+	const [qualifications, setQualifications] = useState("");
+	const [yrsexp, setYrsexp] = useState("");
+	const [pic, setPic] = useState("https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg");
+	const [message, setMessage] = useState(null);
+	const [picMessage, setPicMessage] = useState(null);
+	const [regDate, setRegDate] = useState("");
+
+	const dispatch = useDispatch();
+	const trainerRegistration = useSelector((state) => state.trainerRegistration);
+	const { loading, error } = trainerRegistration;
+
+	const admin_Login = useSelector((state) => state.admin_Login);
+	const { adminInfo } = admin_Login;
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+
+		if (password !== confirmpassword) {
+			setMessage("Passwords do not match");
+		} else {
+			dispatch(
+				trainerRegister(
+					name,
+					dob,
+					nic,
+					gender,
+					telephone,
+					address,
+					email,
+					password,
+					qualifications,
+					yrsexp,
+					pic,
+					regDate
+				)
+			);
+		}
+	};
+	const demoHandler = async (e) => {
+		e.preventDefault();
+
+		setName("John Doe");
+		setDob("1950-06-06");
+		setNic("195045656585");
+		setGender("Male");
+		setTelephone("0777777777");
+		setAddress("Colombo");
+		setEmail("johndoe@gmail.com");
+		setQualifications("International physical trainer certificate");
+		setYrsexp("1 year as a gym instructor");
+		setRegDate("2022-05-19");
+	};
+
+	const resetHandler = async (e) => {
+		e.preventDefault();
+
+		setName("");
+		setDob("");
+		setNic("");
+		setGender("");
+		setTelephone("");
+		setAddress("");
+		setEmail("");
+		setQualifications("");
+		setYrsexp("");
+		setRegDate("");
+	};
+
+	const postDetails = (pics) => {
+		if (pics === "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg") {
+			return setPicMessage("Please Select an Image");
+		}
+		setPicMessage(null);
+		if (pics.type === "image/jpeg" || pics.type === "image/png" || pics.type === "image/jpg") {
+			const data = new FormData();
+			data.append("file", pics);
+			data.append("upload_preset", "trainerProfile");
+			data.append("cloud_name", "bytesquad2022");
+			fetch("https://api.cloudinary.com/v1_1/bytesquad2022/image/upload", {
+				method: "post",
+				body: data,
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					setPic(data.url.toString());
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			return setPicMessage("Please Select an Image");
+		}
+	};
+	return (
+		<div className="registerBg">
+			<MainScreen title="REGISTER - TRAINER">
+				<Button
+					style={{
+						float: "left",
+						marginTop: 5,
+						fontSize: 15,
+					}}
+					href="/admin-trainers"
+				>
+					{" "}
+					Back to Trainers List
+				</Button>
+				<br></br>
+
+				<br></br>
+				<Card
+					className="profileCont"
+					style={{
+						borderRadius: 45,
+						borderWidth: 2.0,
+						marginTop: 20,
+						paddingInline: 10,
+						paddingLeft: 25,
+						paddingRight: 25,
+						background: "rgba(231, 238, 238, 0.8)",
+					}}
+				>
+					<div className="loginContainer">
+						<br></br>
+						<div>
+							{error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+							{message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+							{loading && <Loading />}
+						</div>
+						<br></br>
+						<Row className="trainerProfileContainer">
+							<Col md={6}>
+								<Form onSubmit={submitHandler}>
+									<Form.Group controlId="trainerName">
+										<Form.Label>Name</Form.Label>
+										<Form.Control
+											type="name"
+											value={name}
+											placeholder="Enter name"
+											onChange={(e) => setName(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									<Form.Group controlId="trainerDob">
+										<Form.Label>Date Of Birth</Form.Label>
+										<Form.Control type="date" value={dob} onChange={(e) => setDob(e.target.value)} required />
+									</Form.Group>
+									<Form.Group controlId="trainerFormBasicNic">
+										<Form.Label>NIC Number</Form.Label>
+										<Form.Control
+											type="text"
+											value={nic}
+											placeholder="Enter NIC"
+											onChange={(e) => setNic(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									<div className="form-group">
+										<label className="trainerGender">Gender</label>
+										<select
+											className="form-control"
+											id="trainerGender"
+											value={gender}
+											onChange={(e) => setGender(e.target.value)}
+											required
+										>
+											<option>Select Gender</option>
+											<option value={gender.Male}>Male</option>
+											<option value={gender.Female}>Female</option>
+										</select>
+									</div>
+									<Form.Group controlId="trainerFormBasicTelephone">
+										<Form.Label>Telephone</Form.Label>
+										<Form.Control
+											type="text"
+											value={telephone}
+											placeholder="Enter Telephone Number"
+											onChange={(e) => setTelephone(e.target.value)}
+											required
+											maxLength={10}
+										/>
+									</Form.Group>
+									<Form.Group controlId="trainerFormBasicAddress">
+										<Form.Label>Address</Form.Label>
+										<Form.Control
+											type="textArea"
+											value={address}
+											placeholder="Enter Address"
+											onChange={(e) => setAddress(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									<Form.Group controlId="trainerFormBasicEmail">
+										<Form.Label>Email</Form.Label>
+										<Form.Control
+											type="email"
+											value={email}
+											placeholder="Enter Email Address"
+											onChange={(e) => setEmail(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									<Form.Group controlId="formBasicPassword">
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											type="password"
+											value={password}
+											placeholder="Password"
+											onChange={(e) => setPassword(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									<Form.Group controlId="confirmPassword">
+										<Form.Label>Confirm Password</Form.Label>
+										<Form.Control
+											type="password"
+											value={confirmpassword}
+											placeholder="Confirm Password"
+											onChange={(e) => setConfirmPassword(e.target.value)}
+										/>
+									</Form.Group>
+									<Form.Group controlId="trainerFormBasicQualifications">
+										<Form.Label>Qualifications</Form.Label>
+										<Form.Control
+											type="text"
+											value={qualifications}
+											placeholder="Enter SLDA Register Number"
+											onChange={(e) => setQualifications(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									<Form.Group controlId="trainerFormBasicYrsexp">
+										<Form.Label>Experience</Form.Label>
+										<Form.Control
+											type="text"
+											value={yrsexp}
+											placeholder="Enter Licence Number"
+											onChange={(e) => setYrsexp(e.target.value)}
+											required
+										/>
+									</Form.Group>
+									{picMessage && <ErrorMessage variant="danger">{picMessage}</ErrorMessage>}
+									<Form.Group controlId="pic">
+										<Form.Label>Profile Picture</Form.Label>
+										<Form.File
+											onChange={(e) => postDetails(e.target.files[0])}
+											id="custom-file"
+											type="image/png"
+											label="Upload Profile Picture"
+											custom
+										/>
+									</Form.Group>
+									<Form.Group controlId="trainerRegDate">
+										<Form.Label>Registration Date</Form.Label>
+										<Form.Control type="date" value={regDate} onChange={(e) => setRegDate(e.target.value)} required />
+									</Form.Group>
+									<Button
+										variant="primary"
+										type="submit"
+										style={{
+											fontSize: 15,
+											marginTop: 10,
+										}}
+									>
+										Register
+									</Button>
+									&emsp;
+									<Button
+										variant="danger"
+										onClick={resetHandler}
+										style={{
+											fontSize: 15,
+											marginTop: 10,
+										}}
+									>
+										Reset
+									</Button>
+									&emsp;
+									<Button
+										variant="info"
+										onClick={demoHandler}
+										style={{
+											fontSize: 15,
+											marginTop: 10,
+										}}
+									>
+										Demo
+									</Button>
+								</Form>
+							</Col>
+							<Col
+								style={{
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+								}}
+							>
+								<img
+									src={pic}
+									alt={name}
+									className="profilePic"
+									style={{
+										boxShadow: "7px 7px 20px ",
+										borderColor: "black",
+										borderRadius: 250,
+										background: "white",
+										width: "300px",
+										height: "300px",
+									}}
+								/>
+							</Col>
+						</Row>
+						<br></br>
+					</div>
+				</Card>
+				<br></br>
+			</MainScreen>
+		</div>
+	);
+};
+
+export default TrainerRegisterScreen;
