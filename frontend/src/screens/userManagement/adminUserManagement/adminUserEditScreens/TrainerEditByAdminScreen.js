c3ce06c46fefd67476bfd1b79b2dae792f1cf6a6
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2022-08-18 01:37:23 +0530

    added css to admin user edit screens

--- frontend/src/screens/userManagement/adminUserManagement/adminUserEditScreens/TrainerEditByAdminScreen.js
+++ frontend/src/screens/userManagement/adminUserManagement/adminUserEditScreens/TrainerEditByAdminScreen.js
@@ -7,6 +7,7 @@ import ErrorMessage from "../../../../components/ErrorMessage";
 import { trainerUpdateProfileById } from "../../../../actions/trainerActions";
 import MainScreen from "../../../../components/MainScreen";
 import { authHeader } from "../../../../actions/adminActions";
+import "./adminUserEdit.css";
 
 const TrainerEditByAdminScreen = ({ match }) => {
 	const [name, setName] = useState("");
@@ -123,220 +124,232 @@ const TrainerEditByAdminScreen = ({ match }) => {
 		}
 	}, [match.params.id, adminInfo]);
 
-	return (
-		<div className="registerBg">
-			<MainScreen title="ADMIN EDIT - TRAINER PROFILE">
-				<Button
-					style={{
-						float: "left",
-						marginTop: 5,
-						fontSize: 15,
-					}}
-					href="/admin-trainers"
-				>
-					{" "}
-					Back to Trainers List
-				</Button>
+	if (adminInfo) {
+		return (
+			<div className="adminTrainerEditBg">
 				<br></br>
+				<MainScreen title="ADMIN EDIT - TRAINER PROFILE">
+					<Button
+						variant="success"
+						style={{
+							float: "left",
+							marginTop: 5,
+							fontSize: 15,
+						}}
+						href="/admin-trainers"
+					>
+						{" "}
+						Back to Trainers List
+					</Button>
+					<br></br>
 
-				<br></br>
-				<Card
-					className="profileCont"
-					style={{
-						borderRadius: 45,
-						borderWidth: 2.0,
-						marginTop: 20,
-						paddingInline: 10,
-						paddingLeft: 25,
-						paddingRight: 25,
-						background: "rgba(231, 238, 238, 0.8)",
-					}}
-				>
-					<div className="loginContainer">
-						<br></br>
-						<div>
-							{error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-							{message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
-							{loading && <Loading />}
-						</div>
-						<br></br>
-						<Row className="trainerProfileContainer">
-							<Col md={6}>
-								<Form onSubmit={submitHandler}>
-									<Form.Group controlId="trainerName">
-										<Form.Label>Name</Form.Label>
-										<Form.Control
-											type="name"
-											value={name}
-											placeholder="Enter name"
-											onChange={(e) => setName(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="trainerDob">
-										<Form.Label>Date Of Birth</Form.Label>
-										<Form.Control type="date" value={dob} onChange={(e) => setDob(e.target.value)} required />
-									</Form.Group>
-									<Form.Group controlId="trainerFormBasicNic">
-										<Form.Label>NIC Number</Form.Label>
-										<Form.Control
-											type="text"
-											value={nic}
-											placeholder="Enter NIC"
-											onChange={(e) => setNic(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<div className="form-group">
-										<label className="trainerGender">Gender</label>
-										<select
-											className="form-control"
-											id="trainerGender"
-											value={gender}
-											onChange={(e) => setGender(e.target.value)}
-											required
+					<br></br>
+					<Card
+						className="profileCont"
+						style={{
+							borderRadius: 45,
+							borderWidth: 2.0,
+							marginTop: 20,
+							paddingInline: 10,
+							paddingLeft: 25,
+							paddingRight: 25,
+							background: "rgba(231, 238, 238, 0.8)",
+						}}
+					>
+						<div className="loginContainer">
+							<br></br>
+							<div>
+								{error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+								{message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+								{loading && <Loading />}
+							</div>
+							<br></br>
+							<Row className="trainerProfileContainer">
+								<Col md={6}>
+									<Form onSubmit={submitHandler}>
+										<Form.Group controlId="trainerName">
+											<Form.Label>Name</Form.Label>
+											<Form.Control
+												type="name"
+												value={name}
+												placeholder="Enter name"
+												onChange={(e) => setName(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="trainerDob">
+											<Form.Label>Date Of Birth</Form.Label>
+											<Form.Control type="date" value={dob} onChange={(e) => setDob(e.target.value)} required />
+										</Form.Group>
+										<Form.Group controlId="trainerFormBasicNic">
+											<Form.Label>NIC Number</Form.Label>
+											<Form.Control
+												type="text"
+												value={nic}
+												placeholder="Enter NIC"
+												onChange={(e) => setNic(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<div className="form-group">
+											<label className="trainerGender">Gender</label>
+											<select
+												className="form-control"
+												id="trainerGender"
+												value={gender}
+												onChange={(e) => setGender(e.target.value)}
+												required
+											>
+												<option>Select Gender</option>
+												<option value={gender.Male}>Male</option>
+												<option value={gender.Female}>Female</option>
+											</select>
+										</div>
+										<Form.Group controlId="trainerFormBasicTelephone">
+											<Form.Label>Telephone</Form.Label>
+											<Form.Control
+												type="text"
+												value={telephone}
+												placeholder="Enter Telephone Number"
+												onChange={(e) => setTelephone(e.target.value)}
+												required
+												maxLength={10}
+											/>
+										</Form.Group>
+										<Form.Group controlId="trainerFormBasicAddress">
+											<Form.Label>Address</Form.Label>
+											<Form.Control
+												type="textArea"
+												value={address}
+												placeholder="Enter Address"
+												onChange={(e) => setAddress(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="trainerFormBasicEmail">
+											<Form.Label>Email</Form.Label>
+											<Form.Control
+												type="email"
+												value={email}
+												placeholder="Enter Email Address"
+												onChange={(e) => setEmail(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="formBasicPassword">
+											<Form.Label>Password</Form.Label>
+											<Form.Control
+												type="password"
+												value={password}
+												placeholder="Password"
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</Form.Group>
+										<Form.Group controlId="confirmPassword">
+											<Form.Label>Confirm Password</Form.Label>
+											<Form.Control
+												type="password"
+												value={confirmpassword}
+												placeholder="Confirm Password"
+												onChange={(e) => setConfirmPassword(e.target.value)}
+											/>
+										</Form.Group>
+										<Form.Group controlId="trainerFormBasicQualifications">
+											<Form.Label>Qualifications</Form.Label>
+											<Form.Control
+												type="text"
+												value={qualifications}
+												placeholder="Enter SLDA Register Number"
+												onChange={(e) => setQualifications(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="trainerFormBasicYrsexp">
+											<Form.Label>Experience</Form.Label>
+											<Form.Control
+												type="text"
+												value={yrsexp}
+												placeholder="Enter Licence Number"
+												onChange={(e) => setYrsexp(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										{picMessage && <ErrorMessage variant="danger">{picMessage}</ErrorMessage>}
+										<Form.Group controlId="pic">
+											<Form.Label>Profile Picture</Form.Label>
+											<Form.File
+												onChange={(e) => postDetails(e.target.files[0])}
+												id="custom-file"
+												type="image/png"
+												label="Upload Profile Picture"
+												custom
+											/>
+										</Form.Group>
+										<Form.Group controlId="trainerRegDate">
+											<Form.Label>Registration Date</Form.Label>
+											<Form.Control type="date" value={regDate} onChange={(e) => setRegDate(e.target.value)} required />
+										</Form.Group>
+										<Button
+											variant="primary"
+											type="submit"
+											style={{
+												fontSize: 15,
+												marginTop: 10,
+											}}
 										>
-											<option>Select Gender</option>
-											<option value={gender.Male}>Male</option>
-											<option value={gender.Female}>Female</option>
-										</select>
-									</div>
-									<Form.Group controlId="trainerFormBasicTelephone">
-										<Form.Label>Telephone</Form.Label>
-										<Form.Control
-											type="text"
-											value={telephone}
-											placeholder="Enter Telephone Number"
-											onChange={(e) => setTelephone(e.target.value)}
-											required
-											maxLength={10}
-										/>
-									</Form.Group>
-									<Form.Group controlId="trainerFormBasicAddress">
-										<Form.Label>Address</Form.Label>
-										<Form.Control
-											type="textArea"
-											value={address}
-											placeholder="Enter Address"
-											onChange={(e) => setAddress(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="trainerFormBasicEmail">
-										<Form.Label>Email</Form.Label>
-										<Form.Control
-											type="email"
-											value={email}
-											placeholder="Enter Email Address"
-											onChange={(e) => setEmail(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="formBasicPassword">
-										<Form.Label>Password</Form.Label>
-										<Form.Control
-											type="password"
-											value={password}
-											placeholder="Password"
-											onChange={(e) => setPassword(e.target.value)}
-										/>
-									</Form.Group>
-									<Form.Group controlId="confirmPassword">
-										<Form.Label>Confirm Password</Form.Label>
-										<Form.Control
-											type="password"
-											value={confirmpassword}
-											placeholder="Confirm Password"
-											onChange={(e) => setConfirmPassword(e.target.value)}
-										/>
-									</Form.Group>
-									<Form.Group controlId="trainerFormBasicQualifications">
-										<Form.Label>Qualifications</Form.Label>
-										<Form.Control
-											type="text"
-											value={qualifications}
-											placeholder="Enter SLDA Register Number"
-											onChange={(e) => setQualifications(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="trainerFormBasicYrsexp">
-										<Form.Label>Experience</Form.Label>
-										<Form.Control
-											type="text"
-											value={yrsexp}
-											placeholder="Enter Licence Number"
-											onChange={(e) => setYrsexp(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									{picMessage && <ErrorMessage variant="danger">{picMessage}</ErrorMessage>}
-									<Form.Group controlId="pic">
-										<Form.Label>Profile Picture</Form.Label>
-										<Form.File
-											onChange={(e) => postDetails(e.target.files[0])}
-											id="custom-file"
-											type="image/png"
-											label="Upload Profile Picture"
-											custom
-										/>
-									</Form.Group>
-									<Form.Group controlId="trainerRegDate">
-										<Form.Label>Registration Date</Form.Label>
-										<Form.Control type="date" value={regDate} onChange={(e) => setRegDate(e.target.value)} required />
-									</Form.Group>
-									<Button
-										variant="primary"
-										type="submit"
-										style={{
-											fontSize: 15,
-											marginTop: 10,
-										}}
-									>
-										Update
-									</Button>
-									&emsp;
-									<Button
-										variant="danger"
-										onClick={resetHandler}
-										style={{
-											fontSize: 15,
-											marginTop: 10,
-										}}
-									>
-										Reset
-									</Button>
-								</Form>
-							</Col>
-							<Col
-								style={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<img
-									src={pic}
-									alt={name}
-									className="profilePic"
+											Update
+										</Button>
+										&emsp;
+										<Button
+											variant="danger"
+											onClick={resetHandler}
+											style={{
+												fontSize: 15,
+												marginTop: 10,
+											}}
+										>
+											Reset
+										</Button>
+									</Form>
+								</Col>
+								<Col
 									style={{
-										boxShadow: "7px 7px 20px ",
-										borderColor: "black",
-										borderRadius: 250,
-										background: "white",
-										width: "300px",
-										height: "300px",
+										display: "flex",
+										alignItems: "center",
+										justifyContent: "center",
 									}}
-								/>
-							</Col>
-						</Row>
-						<br></br>
-					</div>
-				</Card>
+								>
+									<img
+										src={pic}
+										alt={name}
+										className="profilePic"
+										style={{
+											boxShadow: "7px 7px 20px ",
+											borderColor: "black",
+											borderRadius: 250,
+											background: "white",
+											width: "300px",
+											height: "300px",
+										}}
+									/>
+								</Col>
+							</Row>
+							<br></br>
+						</div>
+					</Card>
+					<br></br>
+				</MainScreen>
 				<br></br>
-			</MainScreen>
-		</div>
-	);
+			</div>
+		);
+	} else {
+		return (
+			<div className="denied">
+				<MainScreen />
+				<br></br>
+			</div>
+		);
+	}
 };
 
 export default TrainerEditByAdminScreen;
