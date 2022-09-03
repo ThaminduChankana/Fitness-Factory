4055b1e1b971eccdd127920fa2b6e0e4f07ed05f
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-09-03 14:04:23 +0530

    Add admin routes for nutrition plan

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -15,6 +15,7 @@ const {
 	registerCustomer,
 	getCustomerCount,
 } = require("../controllers/customerController");
+const { getNutritionPlans } = require("../controllers/nutritionPlanController");
 const { protect } = require("../middleware/authAdminMiddleware");
 const router = express.Router();
 
@@ -42,5 +43,8 @@ router
 router.route("/customer/profile/edit/:_id").put(protect, updateCustomerProfileById);
 router.route("/customers").get(protect, getCustomers);
 router.route("/customers/count").get(protect, getCustomerCount);
+
+//Routes for admin nutrition plan operations
+router.route("/nutrition_plan/get").get(protect, getNutritionPlans);
 
 module.exports = router;
