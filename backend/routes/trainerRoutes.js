73cdcd0cdd322783eb5f3f664ebde4d705f1c2a8
ChathurikaThathsarani <it20603168@my.sliit.lk>
2022-09-03 14:08:05 +0530

    Add trainer routes for nutrition plan

--- backend/routes/trainerRoutes.js
+++ backend/routes/trainerRoutes.js
@@ -7,7 +7,13 @@ const {
 	deleteTrainerProfile,
 } = require("../controllers/trainerController");
 const { getCustomers, getCustomerProfileById } = require("../controllers/customerController");
-
+const {
+	getNutritionPlans,
+	getNutritionPlanById,
+	createNutritionPlan,
+	updateNutritionPlan,
+	deleteNutritionPlan,
+} = require("../controllers/nutritionPlanController");
 const { protect } = require("../middleware/authTrainerMiddleware");
 const router = express.Router();
 
@@ -21,5 +27,14 @@ router.route("/delete").delete(protect, deleteTrainerProfile);
 //Routes for Customer account operations by trainer
 router.route("/customers").get(protect, getCustomers);
 router.route("/customer/profile/view/:_id").get(protect, getCustomerProfileById);
+
+//Routes for Trainer nutrition plan operations
+router.route("/nutrition_plan/get").get(protect, getNutritionPlans);
+router.route("/nutrition_plan/create").post(protect, createNutritionPlan);
+router
+	.route("/nutrition_plan/get/:id")
+	.get(protect, getNutritionPlanById)
+	.put(protect, updateNutritionPlan)
+	.delete(protect, deleteNutritionPlan);
 
 module.exports = router;
