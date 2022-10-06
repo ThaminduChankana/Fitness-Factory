702c4c09b0a1ca8e201384c4d1b43a74d884340d
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2022-10-06 20:55:46 +0530

    Add admin leave reducers

--- frontend/src/reducers/leaveReducer.js
+++ frontend/src/reducers/leaveReducer.js
@@ -11,6 +11,12 @@ import {
     TRAINERLEAVE_UPDATE_FAIL,
     TRAINERLEAVE_UPDATE_REQUEST,
     TRAINERLEAVE_UPDATE_SUCCESS,
+    ADMINCONFORMLEAVE_LIST_FAIL,
+    ADMINCONFORMLEAVE_LIST_REQUEST,
+    ADMINCONFORMLEAVE_LIST_SUCCESS,
+    ADMINCONFORMLEAVE_UPDATE_FAIL,
+    ADMINCONFORMLEAVE_UPDATE_REQUEST,
+    ADMINCONFORMLEAVE_UPDATE_SUCCESS,
     
   } from "../constants/leaveConstants";
 
@@ -73,12 +79,26 @@ export const TrainerLeaveListReducer = (state = { leave: [] }, action) => {
 
   export const AdminConformLeaveListReducer = (state = { leave: [] }, action) => {
     switch (action.type) {
-      case TRAINERLEAVE_LIST_REQUEST:
+      case ADMINCONFORMLEAVE_LIST_REQUEST:
         return { loading: true };
-      case TRAINERLEAVE_LIST_SUCCESS:
+      case ADMINCONFORMLEAVE_LIST_SUCCESS:
         return { loading: false, trainerLeave: action.payload };
-      case TRAINERLEAVE_LIST_FAIL:
+      case ADMINCONFORMLEAVE_LIST_FAIL:
         return { loading: false, error: action.payload };
+  
+      default:
+        return state;
+    }
+  };
+
+  export const AdminConformLeaveUpdateReducer = (state = {}, action) => {
+    switch (action.type) {
+      case ADMINCONFORMLEAVE_UPDATE_REQUEST:
+        return { loading: true };
+      case ADMINCONFORMLEAVE_UPDATE_SUCCESS:
+        return { loading: false, success: true };
+      case ADMINCONFORMLEAVE_UPDATE_FAIL:
+        return { loading: false, error: action.payload, success: false };
   
       default:
         return state;
