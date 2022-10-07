e26fd201ea3fc8933e9cb2353b19196952dd7a7a
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2022-10-07 14:56:57 +0530

    add more details for terms and condition

--- frontend/src/screens/static/termsAndConditions/TermsAndCondition.js
+++ frontend/src/screens/static/termsAndConditions/TermsAndCondition.js
@@ -47,206 +47,107 @@ function TermsAndCondtions() {
 						<br></br>
 						<h2 style={{fontSize:40}}> Membership</h2>
 						<ul>
-							<li
+							<div
 								style={{
 									fontSize: 20,
 								}}
 							>
-								All memberships are Non-Transferable & Non-Refundable.
-                                All members must be over 16 to avail of facilities and services provided at Goldstone Gym & Fitness.
-                                Goldstone Gym & Fitness is a private members club and the management reserve the right to refuse membership to any individual without cause. Refrences may be required on a case by case basis and Garda checks will also be run on a case by case basis. Failure to pass a Garda check will result in membership termination and the refusal of entry again.
-                                Covid-19 restrictions, members will be required to follow government guidance and advice from the National Public Health Emergency Team (NPHET). These restrictions may include reduced services, restricted access and or temporary closure of the club. In the event of any government ordered temporary closure all memberships will be frozen for the closed period and will restart upon reopening; no time will be lost on membership agreements and no fees will be deducted during closure. Refunds will not be available as a result of Covid-19 restrictions or closures. 
-                                Membership may be renewed on payment of the prevailing fee and in accordance with the terms and conditions then in force.
-                                By joining the club, members automatically accept and agree to be bound by these conditions of membership.
-                                The club may withdraw use of all or part of the club for the purpose of undertaking maintenance work or any other work considered necessary for a maximum period of seven working days.
-                                All members must scan their finger for access to the building on each visit to the club.
-                                Entry will only be permitted to those with a valid membership, in date and in credit. If you have an amount due to be paid on your account you won’t be able to enter the building.
-                                As a member you agree to comply with the rules of the club with regards to use of the facilities, opening hours and your conduct. The club may make reasonable changes to these rules, from time to time, provided the club gives advance notice of these changes.
-                                You may, at your own discretion and expense, obtain personal insurance for loss, injury or damage that you might sustain arising from use of the club. You exercise at your own discretion and accept any injury or illness brought on by exercise is your own responsibility.
-                                Members wishing to report on accidents, incidents, or problems with services at the club should contact the Duty Manager (simply ask at reception desk upstairs) or can email join@goldstonefitness.ie in confidence.
-                                In the event of a breach of membership rules and regulations the Club reserves the right of admission and may reserve the right to require any member or guest to leave the premises. Any member found in breach of rules or commiting an illegal act, including theft, will be asked to permanently leave the club and be barred from ever entering the club again. 
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								These terms and conditions may be updated at our discretion without prior notice.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								It is your responsibility to read and understand these terms and conditions, a written copy of which is
-								available on request
-							</li>
+								<b>All memberships are Non-Transferable & Non-Refundable</b><br/><br/>
+                                All members must be over 16 to avail of facilities and services provided at Fitness Factory Gym & Fitness.<br/><br/>
+                                Fitness Factory Gym & Fitness is a private members club and the management reserve the right to refuse membership to any individual without cause. Refrences may be required on a case by case basis and Garda checks will also be run on a case by case basis. Failure to pass a Garda check will result in membership termination and the refusal of entry again.<br/><br/>
+                                Covid-19 restrictions, members will be required to follow government guidance and advice from the National Public Health Emergency Team (NPHET). These restrictions may include reduced services, restricted access and or temporary closure of the club. In the event of any government ordered temporary closure all memberships will be frozen for the closed period and will restart upon reopening; no time will be lost on membership agreements and no fees will be deducted during closure. Refunds will not be available as a result of Covid-19 restrictions or closures. <br/><br/>
+                                Membership may be renewed on payment of the prevailing fee and in accordance with the terms and conditions then in force.<br/><br/>
+                                By joining the club, members automatically accept and agree to be bound by these conditions of membership.<br/><br/>
+                                The club may withdraw use of all or part of the club for the purpose of undertaking maintenance work or any other work considered necessary for a maximum period of seven working days.<br/><br/>
+                                All members must scan their finger for access to the building on each visit to the club.<br/><br/>
+                                Entry will only be permitted to those with a valid membership, in date and in credit. If you have an amount due to be paid on your account you won’t be able to enter the building.<br/><br/>
+                                As a member you agree to comply with the rules of the club with regards to use of the facilities, opening hours and your conduct. The club may make reasonable changes to these rules, from time to time, provided the club gives advance notice of these changes.<br/><br/>
+                                You may, at your own discretion and expense, obtain personal insurance for loss, injury or damage that you might sustain arising from use of the club. You exercise at your own discretion and accept any injury or illness brought on by exercise is your own responsibility.<br/><br/>
+                                Members wishing to report on accidents, incidents, or problems with services at the club should contact the Duty Manager (simply ask at reception desk upstairs) or can email join@goldstonefitness.ie in confidence.<br/><br/>
+                                In the event of a breach of membership rules and regulations the Club reserves the right of admission and may reserve the right to require any member or guest to leave the premises. Any member found in breach of rules or commiting an illegal act, including theft, will be asked to permanently leave the club and be barred from ever entering the club again. <br/><br/>
+							</div>
+							
+							
 						</ul>
 						<br></br>
 						<br></br>
-						<h2> MAKING BOOKINGS</h2>
+						<h2 style={{fontSize:40}}>Bank Card Direct Debit Payments</h2>
 						<ul>
-							<li
+							<div
 								style={{
 									fontSize: 20,
 								}}
 							>
-								You may book appointments with us in person, by telephone or by email.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								Advanced bookings may be refused to clients who have missed appointments without giving at least 48
-								hours notice.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								The contents of the clinic diary are confidential. You should not attempt to view the diary when making
-								a booking in person.
-							</li>
+								On selecting the Pay Monthly membership options, members will be required to pay the associated registration fee (if applicable) and the 1st-month membership fee in advance. Members are required to agree to the normal terms and conditions of payment.<br/><br/>
+                                This is a recurring monthly charge. Membership Fees will be debited from the member's account, using their given bank debit/credit card, on or around their join date each month. The club may offer various  MINIMUM FIXED TERM AGREEMENTS on Pay Monthly memberships these can range from 3 months to 12 months depending on the offer at the time.  Members who have chosen a FIXED TERM pay monthly agreement will NOT have a cancel option until after the Fixed Term, after that time they will have the option to cancel at any time.<br/><br/>
+                                Members who have chosen the Pay Monthly options with NO FIXED TERM can cancel or freeze their agreement at any time, by simply logging in to their Member Area online and pressing the "Cancel" button.<br/>
+                                Members are required to keep their account in funds to meet these payments.  In the event of the failure to pay the monthly membership fee, missed payments will be re-submitted for payment by Goldstone Fitness Limited. Goldstone Fitness Limited reserves the right to cancel membership if payment is not received. Access to the facility will be denied until said payment is received and if payment is not received, and cancellation occurs, the registration fee becomes applicable again on re-joining.<br/><br/>
+                                These arrears of fees must be paid by Credit or Debit card online using the Member Area.<br/><br/>
+                                Failure to meet direct debit fee – In the event of the failure to pay the monthly direct debit fee, you will first be notified that your membership has been temporarily stopped until the balance is paid.<br/><br/>
+                                
+							</div>
+							
 						</ul>
 						<br></br>
 						<br></br>
-						<h2> ARRIVAL TIMES</h2>
+						<h2 style={{fontSize:40}}>Lockers</h2>
 						<ul>
-							<li
+							<div
 								style={{
 									fontSize: 20,
 								}}
 							>
-								We recommend that you arrive before your appointment time, allowing sufficient travel time during the
-								traffic rush hours
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								If you arrive more than 10 minutes late for your appointment, we may reschedule your treatment for
-								another time and you may be charged a late cancellation fee.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								If you arrive late and agree to receive a shorter treatment than normal, you agree to pay the full cost
-								of that treatment.
-							</li>
+								Lockers are provided for the use of members and their guests.<br/><br/>
+                                Lockers may not be used overnight. Members who do so are liable to find that the lockers are opened and locker contents removed. Removal of such items is strictly at the members own risk.<br/><br/>
+                                Padlocks may be purchased at reception.<br/><br/>
+                                In the interest of safety and security please use the lockers provided. The club will have no liability for goods taken from the changing rooms or lockers.<br/><br/>
+							</div>
+							
+							
 						</ul>
 						<br></br>
 						<br></br>
-						<h2> REFUNDS</h2>
+						<h2 style={{fontSize:40}}>Opening Hours</h2>
 						<ul>
-							<li
+							<div
 								style={{
 									fontSize: 20,
 								}}
 							>
-								At our discretion, we can offer a full refund on retail products if they are unused, in their original
-								packaging, in a fully saleable condition and are accompanied by proof of purchase, presented within 28
-								days of purchase.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								We do not offer a refund for treatments unless we acknowledge that the service received was below the
-								professional standard we strive to achieve.
-							</li>
+								The club reserves the right to vary the opening hours as considered necessary for the proper operation of the centre.
+							</div>
+							
 						</ul>
 						<br></br>
 						<br></br>
-						<h2> PAYMENT</h2>
+						<h2 style={{fontSize:40}}> Disclaimer Of Liability For Legal Declarations</h2>
+					
 						<ul>
-							<li
+							<div
 								style={{
 									fontSize: 20,
 								}}
 							>
-								We may ask you for full payment, or a deposit in advance, in order to secure any booking. This payment
-								is non-refundable in the absence of mitigating circumstances but may be put towards the cost of future
-								treatment or any cancellation charges that apply.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								Mitigating circumstances include genuine, unforeseen ill health or personal crisis.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								In the majority of cases, payment is completed on the day of treatment. If you do not pay in full on the
-								day of your treatment, we will not accept further bookings until any outstanding amounts have been paid
-								in full.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								We accept payment by cash, or debit & credit cards (but not cheques or Lavish/Tesco vouchers). Receipts
-								for debit & credit cards can be provided as an email or SMS text message. A printed receipt can be
-								requested if needed.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								If your treatment relates to an insurance claim and we accept you as a patient, having agreed fees in
-								advance with your insurer, we will invoice your insurer after you have been discharged (you will not
-								need to pay up front).
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								If you have cover with a private (cash-back) health scheme - e.g. SimplyHealth - you will need to pay
-								for each treatment and claim back the cost yourself (a receipt will be provided for this purpose). To
-								avoid disappointment, please check with your insurer before receiving treatment that the therapy is
-								covered by your scheme. Podiatry and Osteopathy treatments should be covered by all health insurance
-								schemes.
-							</li>
+								If any provisions of these terms and conditions are found to be illegal, outdated, less legally preferable in a legal action or otherwise unenforceable by reason of the laws of any state or country in which these terms are intended to be effective, then to the extent and within the jurisdiction in which that term is illegal, invalid or unenforceable, it shall be severed and deleted from these terms and the remaining terms shall survive, remain in full force and effect. We refuse to accept liability for parties who do not make us openly aware of faults and later deny such observations in legal action - Access our website at your own, sole risk and liability without exception.<br/><br/>
+                                These terms shall be governed by and interpreted in accordance with the laws of Ireland.
+							</div>
+							
 						</ul>
 						<br></br>
 						<br></br>
-						<h2> COMPLAINTS</h2>
+						<h2 style={{fontSize:40}}> Terms and Conditions for accessing this website</h2>
+						
 						<ul>
-							<li
+							<div
 								style={{
 									fontSize: 20,
 								}}
 							>
-								If you have a complaint about the service you have received at Absolute Health please speak to one of
-								the clinic managers.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								If the clinic manager is unable to resolve your complaint at the time, you may put your complaint in
-								writing to the directors of BB Osteopaths Limited for further consideration.
-							</li>
-							<li
-								style={{
-									fontSize: 20,
-								}}
-							>
-								If your complaint relates to Osteopathic treatment please request a separate Complaints Procedure form.
-							</li>
+								 1. By using this site you agree to be legally bound by these terms and conditions. If you do not agree to be legally bound by all the following terms you should not access and/or use our website or communicate in any way with the server hosting this website, namely, but not limited to,www.goldstonefitness.ie<br/><br/>2. Henceforth in this legal declaration, you the website user, or associated parties will be known as either "you" or "the client". We, Goldstone Fitness Ltd. will be known as "We", "Us" or "Our Company", our general or individual assets will be addressed by the prefix "Our".<br/><br/>
+                                 3. You agree that we reserve the right to change these, or any, terms and conditions, content and/or declarations without prior notification, and changes made come into immediate effect upon being presented here. When such changes are made, you may revoke your agreement by engaging with our company online using this or any website, or continue to accept such changes
+							</div>
+							
+							
 						</ul>
 						<br></br>
 						<br></br>
